e090800238af5f0ef71998db1d289c683eed76fd
Chris Dundon <chris.dundon@gmail.com>
2018-03-30 18:11:00 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,20 @@
 def oxford_comma(array)
   l_idx = array.size - 1 
-  if l_idx > 0
+  if l_idx == 0
+    return array[0]
+  end
+
+  mod = array[l_idx]
+  mod = "and " + mod
+  array.pop
+  array << mod
+  
+  if l_idx == 1
     mod = array[l_idx]
     mod = "and " + mod
     array.pop
     array << mod
-    array.join(", ")
+    array.join(" ")
   else
     array[0]
   end
