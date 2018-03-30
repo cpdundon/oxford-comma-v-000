41cab9e7f91fd79913ab0e235f47c49b5ffb3674
Chris Dundon <chris.dundon@gmail.com>
2018-03-30 18:01:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,10 @@
 def oxford_comma(array)
   l_idx = array.size - 1 
-  mod = array[l_idx]
-  mod = "and " + mod
-  array.pop
-  arr << mod
+  if l_idx > 0
+    mod = array[l_idx]
+    mod = "and " + mod
+    array.pop
+    arr << mod
+  end
   arr.to_s(", ")
 end
